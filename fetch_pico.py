f782247ce8d4539bfdb870b7dcd6f43f8eaa7978
genus-lang <rinku4578914@gmail.com>
2026-04-04 17:04:35 +0530

    feat: implement smart auto-capture and face detection for YouTube slide screenshots

---
+++ fetch_pico.py
@@ -0,0 +1,3 @@
+﻿from urllib.request import urlopen
+html = urlopen('https://raw.githubusercontent.com/tehnokv/picojs/master/cam.html').read().decode('utf-8')
+print('\n'.join([line for line in html.split('\n') if 'pico' in line or 'facefinder' in line][:20]))
