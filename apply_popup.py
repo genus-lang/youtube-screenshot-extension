98a2f74674f976773c7af4726ee1c6cdfc52c0ea
genus-lang <rinku4578914@gmail.com>
2026-04-04 17:04:11 +0530

    feat: implement screenshot note-taking modal with Tesseract OCR integration and UI injection system

---
+++ apply_popup.py
@@ -0,0 +1,15 @@
+﻿import re
+
+with open('src/popup/popup.js', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+text = text.replace('setTimeout(() => {\\n          chrome.tabs.create({ url: resp.links.oneNoteWebUrl.href });\\n        }, 1500);', 
+'''btnOpenOneNote.classList.remove(\\'hidden\\');
+          btnOpenOneNote.onclick = () => chrome.tabs.create({ url: resp.links.oneNoteWebUrl.href });''')
+
+text = text.replace('btnExportOneNote.innerHTML = \\'<span class=\"spinner\"></span> Syncing...\\';',
+'''btnExportOneNote.innerHTML = \\'<span class=\"spinner\"></span> Syncing...\\';
+    btnOpenOneNote.classList.add(\\'hidden\\');''')
+
+with open('src/popup/popup.js', 'w', encoding='utf-8') as f:
+    f.write(text)
